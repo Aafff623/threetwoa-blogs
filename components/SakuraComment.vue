32c07e4cf1baf0f29bd7f2790b31103dd2c4b88a
Aafff623 <1012512411@qq.com>
2026-06-19 13:44:48 +0800

    fix(comment): remove ClientOnly wrapper around Giscus

--- components/SakuraComment.vue
+++ components/SakuraComment.vue
@@ -3,24 +3,22 @@ import Giscus from '@giscus/vue'
 </script>
 
 <template>
-  <ClientOnly>
-    <SakuraCard w="full" class="sakura-comment" mt-6>
-      <Giscus
-        repo="Aafff623/threetwoa-blogs"
-        repo-id="R_kgDOS-UR5w"
-        category="Announcements"
-        category-id="DIC_kwDOS-UR584C_dOM"
-        mapping="pathname"
-        strict="0"
-        reactions-enabled="1"
-        emit-metadata="0"
-        input-position="bottom"
-        theme="preferred_color_scheme"
-        lang="zh-CN"
-        loading="lazy"
-      />
-    </SakuraCard>
-  </ClientOnly>
+  <SakuraCard w="full" class="sakura-comment" mt-6>
+    <Giscus
+      repo="Aafff623/threetwoa-blogs"
+      repo-id="R_kgDOS-UR5w"
+      category="Announcements"
+      category-id="DIC_kwDOS-UR584C_dOM"
+      mapping="pathname"
+      strict="0"
+      reactions-enabled="1"
+      emit-metadata="0"
+      input-position="bottom"
+      theme="preferred_color_scheme"
+      lang="zh-CN"
+      loading="lazy"
+    />
+  </SakuraCard>
 </template>
 
 <style lang="scss" scoped>
